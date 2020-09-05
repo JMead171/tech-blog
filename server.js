6e890824e47ea1c4d8b2e4aae438f42628a339d2
Jeff Mead <jeffdmead171@gmail.com>
2020-09-05 14:02:56 -0400

    fixed bugs

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: { maxAge: 60 * 1000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
